6bbb2b2a8fccaf5eeffc47ecda80842bd4cd4267
AnierinB <anierin@evolution-x.org>
2022-07-03 13:53:00 +0900

    avicii: Configure & ship OPlusExtras
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,7 +93,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest_odm.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_avicii
